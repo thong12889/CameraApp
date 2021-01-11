4e2c6732289059a90749ada8a55da8eeb76d7989
thong12889 <thongchai_12889@hotmail.com>
2021-01-12 08:03:05 +0900

    first commit

---
+++ USBCamApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace USBCamApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnOneShot = new System.Windows.Forms.Button();
+            this.btnContinuousShot = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(30, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(464, 326);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnOneShot
+            // 
+            this.btnOneShot.Location = new System.Drawing.Point(45, 401);
+            this.btnOneShot.Name = "btnOneShot";
+            this.btnOneShot.Size = new System.Drawing.Size(75, 23);
+            this.btnOneShot.TabIndex = 1;
+            this.btnOneShot.Text = "OneShot";
+            this.btnOneShot.UseVisualStyleBackColor = true;
+            this.btnOneShot.Click += new System.EventHandler(this.btnOneShot_Click);
+            // 
+            // btnContinuousShot
+            // 
+            this.btnContinuousShot.Location = new System.Drawing.Point(152, 401);
+            this.btnContinuousShot.Name = "btnContinuousShot";
+            this.btnContinuousShot.Size = new System.Drawing.Size(75, 23);
+            this.btnContinuousShot.TabIndex = 2;
+            this.btnContinuousShot.Text = "Continuous";
+            this.btnContinuousShot.UseVisualStyleBackColor = true;
+            this.btnContinuousShot.Click += new System.EventHandler(this.btnContinuousShot_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(254, 401);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(370, 401);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(159, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 19);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 478);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnContinuousShot);
+            this.Controls.Add(this.btnOneShot);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "USB Camera App";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnOneShot;
+        private System.Windows.Forms.Button btnContinuousShot;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ USBCamApp1/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Basler.Pylon;
+using System.IO;
+
+namespace USBCamApp1
+{
+    public partial class Form1 : Form
+    {
+        baslerCamcs camera = new baslerCamcs();
+        public Form1()
+        {
+            InitializeComponent();
+            camera.CameraImageEvent += Camera_CameraImageEvent;
+
+        }
+
+        private void Camera_CameraImageEvent(Bitmap bmp)
+        {
+            pictureBox1.Invoke(new MethodInvoker(delegate
+            {
+                Bitmap old = pictureBox1.Image as Bitmap;
+                pictureBox1.Image = bmp;
+                if (old != null)
+                {
+                    old.Dispose();
+                }
+            }));
+        }
+
+        void Unanble()
+        {
+            btnStop.Enabled = false;
+            btnOneShot.Enabled = false;
+            btnContinuousShot.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(camera.CameraNumber > 0)
+            {
+                camera.CameraInit();
+            }
+            else
+            {
+                MessageBox.Show("Not connect to camera");
+                Unanble();
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            camera.Stop();
+        }
+
+        private void btnOneShot_Click(object sender, EventArgs e)
+        {
+            camera.OneShot();
+        }
+
+        private void btnContinuousShot_Click(object sender, EventArgs e)
+        {
+            camera.KeepShot();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            camera.DestroyCamera();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Directory.Exists(@"C:\Users\DEMO1\source\repos\USBCamApp1\Capture"))
+                {
+                    Directory.CreateDirectory(@"C:\Users\DEMO1\source\repos\USBCamApp1\Capture");
+                    MessageBox.Show("Images floder created...");
+                }
+                else
+                {
+                    string path = @"C:\Users\DEMO1\source\repos\USBCamApp1\Capture";
+                    pictureBox1.Image.Save(path + @"\" + textBox1.Text + ".jpg", ImageFormat.Jpeg);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+    public class baslerCamcs
+    {
+        public int CameraNumber = CameraFinder.Enumerate().Count();
+        public delegate void CameraImage(Bitmap bmp);
+        public event CameraImage CameraImageEvent;
+
+        Camera camera;
+
+        PixelDataConverter pxConvert = new PixelDataConverter();
+
+        bool GrabOver = false;
+
+        public void CameraInit()
+        {
+            camera = new Camera();
+            camera.CameraOpened += Configuration.AcquireContinuous;
+            camera.ConnectionLost += Camera_ConnectionLost;
+            camera.StreamGrabber.GrabStarted += StreamGrabber_GrabStarted;
+            camera.StreamGrabber.ImageGrabbed += StreamGrabber_ImageGrabbed;
+            camera.StreamGrabber.GrabStopped += StreamGrabber_GrabStopped;
+
+            camera.Open();
+        }
+
+        private void StreamGrabber_GrabStopped(object sender, GrabStopEventArgs e)
+        {
+            GrabOver = false;
+        }
+
+        private void StreamGrabber_ImageGrabbed(object sender, ImageGrabbedEventArgs e)
+        {
+            IGrabResult grabResult = e.GrabResult;
+            if (grabResult.IsValid)
+            {
+                if (GrabOver)
+                    CameraImageEvent(GrabResult2Bmp(grabResult));
+            }
+        }
+
+        private void Camera_ConnectionLost(object sender, EventArgs e)
+        {
+            camera.StreamGrabber.Stop();
+            DestroyCamera();
+        }
+
+        private void StreamGrabber_GrabStarted(object sender, EventArgs e)
+        {
+            GrabOver = true;
+        }
+
+        public void OneShot()
+        {
+            if(camera != null)
+            {
+                camera.Parameters[PLCamera.AcquisitionMode].SetValue(PLCamera.AcquisitionMode.SingleFrame);
+                camera.StreamGrabber.Start(1, GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+            }
+        }
+
+        public void KeepShot()
+        {
+            if(camera != null)
+            {
+                camera.Parameters[PLCamera.AcquisitionMode].SetValue(PLCamera.AcquisitionMode.Continuous);
+                camera.StreamGrabber.Start(GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+            }
+        }
+
+        public void Stop()
+        {
+            if(camera != null)
+            {
+                camera.StreamGrabber.Stop();
+            }
+        }
+
+        Bitmap GrabResult2Bmp(IGrabResult grabResult)
+        {
+            Bitmap b = new Bitmap(grabResult.Width, grabResult.Height, PixelFormat.Format32bppRgb);
+            BitmapData bmpData = b.LockBits(new Rectangle(0, 0, b.Width, b.Height), ImageLockMode.ReadWrite, b.PixelFormat);
+            pxConvert.OutputPixelFormat = PixelType.BGRA8packed;
+            IntPtr bmpIntpr = bmpData.Scan0;
+            pxConvert.Convert(bmpIntpr, bmpData.Stride * b.Height, grabResult);
+            b.UnlockBits(bmpData);
+            return b;
+        }
+
+        public void DestroyCamera()
+        {
+            if(camera != null)
+            {
+                camera.Close();
+                camera.Dispose();
+                camera = null;
+            }
+        }
+    }
+   
+}
